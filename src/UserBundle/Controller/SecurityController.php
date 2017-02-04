0121260fb2ca135cb64c9b5db53a4f04fcbc7ce0
yomat <matthias.mailliard@gmail.com>
2017-02-04 17:15:15 +0100

    setting default redirection route on login success

--- src/UserBundle/Controller/SecurityController.php
+++ src/UserBundle/Controller/SecurityController.php
@@ -18,7 +18,7 @@ class SecurityController extends Controller
     {
         // Si le visiteur est déjà identifié, on le redirige vers l'accueil
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            return $this->redirectToRoute('platform_accueil');
+            return $this->redirectToRoute('app');
         }
 
         // Le service authentication_utils permet de récupérer le nom d'utilisateur
